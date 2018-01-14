1882d1340c9c5cbec6590b90634a3d0fc0a9b886
Emily Morehouse <emily@cuttlesoft.com>
2018-01-14 15:34:28 -0700

    - Updating eslint preferences, supporting ES2017 - Adding prettier - Lint command no longer exists 0, as we want to know when this fails. - Updates code to adhere to linting, with the exception of test file (which needs to be merged).

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {AppContainer} from 'react-hot-loader'
-import Routes from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader'; // eslint-disable-line import/no-extraneous-dependencies
+import Routes from './routes';
+import { App } from './app';
 
 ReactDOM.render(
   <AppContainer>
-      <Routes />
+    <Routes />
   </AppContainer>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
 
 // Hot Module Replacement API
 if (module.hot) {
   module.hot.accept('./app', () => {
-    const NextApp = require('./app').default;
     ReactDOM.render(
       <AppContainer>
-        <NextApp/>
+        <App />
       </AppContainer>,
-      document.getElementById('app')
+      document.getElementById('app'),
     );
   });
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
 } from 'react-router-dom';
+
 import App from './app';
 import About from './components/About';
 import Sample from './components/Sample';
-import 'styles/index.scss';
+import '../styles/index.scss';
 
 const Routes = () => (
   <Router>
     <div>
-      <Route exact path="/" component={App}/>
-      <Route path="/about" component={About}/>
-      <Route path="/sample" component={Sample}/>
+      <Route exact path="/" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/sample" component={Sample} />
     </div>
   </Router>
 );
