462f0049c94644759a2ff819707909dd2a4ff2bc
Emily Morehouse <emily@cuttlesoft.com>
2018-01-14 17:04:18 -0700

    Linting webpack files.

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -2,46 +2,46 @@ module.exports = [
   {
     test: /\.jsx?$/,
     exclude: /(node_modules|bower_components|public\/)/,
-    loader: "babel-loader"
+    loader: 'babel-loader',
   },
   {
     test: /\.css$/,
     loaders: ['style-loader', 'css-loader?importLoaders=1'],
-    exclude: ['node_modules']
+    exclude: ['node_modules'],
   },
   {
     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
     exclude: /(node_modules|bower_components)/,
-    loader: "file-loader"
+    loader: 'file-loader',
   },
   {
     test: /\.(woff|woff2)$/,
     exclude: /(node_modules|bower_components)/,
-    loader: "file-loader?prefix=font/&limit=5000"
+    loader: 'file-loader?prefix=font/&limit=5000',
   },
   {
     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
     exclude: /(node_modules|bower_components)/,
-    loader: "file-loader?limit=10000&mimetype=application/octet-stream"
+    loader: 'file-loader?limit=10000&mimetype=application/octet-stream',
   },
   {
     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
     exclude: /(node_modules|bower_components)/,
-    loader: "file-loader?limit=10000&mimetype=image/svg+xml"
+    loader: 'file-loader?limit=10000&mimetype=image/svg+xml',
   },
   {
     test: /\.gif/,
     exclude: /(node_modules|bower_components)/,
-    loader: "file-loader?limit=10000&mimetype=image/gif"
+    loader: 'file-loader?limit=10000&mimetype=image/gif',
   },
   {
     test: /\.jpg/,
     exclude: /(node_modules|bower_components)/,
-    loader: "file-loader?limit=10000&mimetype=image/jpg"
+    loader: 'file-loader?limit=10000&mimetype=image/jpg',
   },
   {
     test: /\.png/,
     exclude: /(node_modules|bower_components)/,
-    loader: "file-loader?limit=10000&mimetype=image/png"
-  }
+    loader: 'file-loader?limit=10000&mimetype=image/png',
+  },
 ];
