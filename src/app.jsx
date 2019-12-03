b6fc64ab8091ec692b1f1ebb534533e92f9867c5
Emily Morehouse <emily@cuttlesoft.com>
2019-12-03 13:42:54 -0700

    [#159062244] 🔨 (Navigation) Move Navigation back to `components`

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
-import Navigation from './pages/Navigation'
+import Navigation from './components/Navigation'
 import colors from './colors'
 import theme from './theme'
 
