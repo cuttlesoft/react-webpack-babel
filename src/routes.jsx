d644ef7b9f8009348007ef3d117ef31a3ed460cc
Emily Morehouse <emily@cuttlesoft.com>
2019-12-03 13:28:52 -0700

    [#159062244] 🚜 (Base) Rename `components` directory to `pages`; add placeholder `components` directory

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import App from './app'
-import About from './components/About'
-import Sample from './components/Sample'
+import About from './pages/About'
+import Sample from './pages/Sample'
 
 const Routes = () => (
   <Router>
